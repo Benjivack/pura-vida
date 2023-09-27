6bc42b0798eab6d5806bdc1f23dede8ae0a913a8
Benjamin Litvack <ben.litvack1@gmail.com>
2023-09-27 20:00:58 +0000

    Resolve "Create posts table"

--- api/migrations/002_create_tables.py
+++ api/migrations/002_create_tables.py
@@ -3,15 +3,33 @@ steps = [
         """
         CREATE TABLE users (
             id SERIAL PRIMARY KEY NOT NULL,
-            username VARCHAR(10) NOT NULL,
+            username VARCHAR(10) NOT NULL UNIQUE,
             password VARCHAR(20) NOT NULL,
-            email VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL UNIQUE,
             role VARCHAR(9) NOT NULL,
             joined TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
         );
         """,
         """
         DROP TABLE users;
+        """
+    ],
+
+    [
+        """
+        CREATE TABLE posts (
+            id SERIAL PRIMARY KEY NOT NULL,
+            title VARCHAR (100) NOT NULL,
+            latitude DECIMAL (9,6) NOT NULL,
+            longitude DECIMAL (9,6) NOT NULL,
+            zipcode NUMERIC (5) NOT NULL,
+            body TEXT NOT NULL,
+            created_by INTEGER REFERENCES users NOT NULL,
+            created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+        """,
+        """
+        DROP TABLE posts;
         """
     ]
 ]
