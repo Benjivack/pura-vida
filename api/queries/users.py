3e5e29c1c059b13c4459ed84db9a73330471b77e
Benjamin Litvack <ben.litvack1@gmail.com>
2023-10-09 23:16:59 +0000

    Resolve "Create get_all endpoint for Users"

--- api/queries/users.py
+++ api/queries/users.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel
 from datetime import date
 from queries.accounts import pool
+from typing import List, Union
+
+
+class Error(BaseModel):
+    message: str
 
 
 class UserIn(BaseModel):
@@ -21,26 +26,56 @@ class UserOut(BaseModel):
 
 
 class UserRepository:
+    def get_all(self) -> Union[Error, List[UserOut]]:
+        try:
+            with pool.connection() as conn:
+
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, username, password, email, role, joined
+                        FROM users
+                        ORDER BY joined;
+                        """
+                    )
+                    result = []
+                    for record in db:
+                        user = UserOut(
+                                id=record[0],
+                                username=record[1],
+                                password=record[2],
+                                email=record[3],
+                                role=record[4],
+                                joined=record[5],
+                        )
+                        result.append(user)
+                    return result
+        except Exception:
+            return {"message": "could not get all users"}
+
     def create(self, user: UserIn):
-        with pool.connection() as conn:
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    INSERT INTO users
-                        (username, password, email, role, joined)
-                    VALUES
-                        (%s, %s, %s, %s, %s)
-                    RETURNING id;
-                    """,
-                    [
-                        user.username,
-                        user.password,
-                        user.email,
-                        user.role,
-                        user.joined
-                    ]
-                )
-                print('RESULT ID: ', result)
-                id = result.fetchone()[0]
-                old_data = user.dict()
-                return UserOut(id=id, **old_data)
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO users
+                            (username, password, email, role, joined)
+                        VALUES
+                            (%s, %s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            user.username,
+                            user.password,
+                            user.email,
+                            user.role,
+                            user.joined
+                        ]
+                    )
+                    print('RESULT ID: ', result)
+                    id = result.fetchone()[0]
+                    old_data = user.dict()
+                    return UserOut(id=id, **old_data)
+        except Exception:
+            return {"message": "no create user"}
--- api/routers/users.py
+++ api/routers/users.py
@@ -1,10 +1,28 @@
-from fastapi import APIRouter, Depends
-from queries.users import UserIn, UserRepository
+from fastapi import APIRouter, Depends, Response
+from typing import Union, List
+from queries.users import (
+    Error,
+    UserIn,
+    UserRepository,
+    UserOut,
+    )
 
 
 router = APIRouter()
 
 
-@router.post("/api/users")
-def create_user(user: UserIn, repo: UserRepository = Depends()):
+@router.post("/api/users", response_model=Union[UserOut, Error])
+def create_user(
+    user: UserIn,
+    response: Response,
+    repo: UserRepository = Depends(),
+):
+    # response.status_code = 400
     return repo.create(user)
+
+
+@router.get("/users", response_model=Union[List[UserOut], Error])
+def get_all(
+    repo: UserRepository = Depends(),
+):
+    return repo.get_all()
