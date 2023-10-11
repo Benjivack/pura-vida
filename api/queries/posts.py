2452a144d5aa220820766206066e2ecbf6aeef9e
Brian La <brianlax23@gmail.com>
2023-10-11 02:13:33 +0000

    Resolve "Get all posts"

--- api/queries/posts.py
+++ api/queries/posts.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel
 from datetime import date
 from queries.accounts import pool
+from typing import List, Union
+
+
+class Error(BaseModel):
+    message: str
 
 
 class PostIn(BaseModel):
@@ -56,3 +61,40 @@ class PostRepository:
                 id = result.fetchone()[0]
                 old_data = posts.dict()
                 return PostOut(id=id, **old_data)
+
+    def get_all(self) -> Union[List[PostOut], Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT (
+                            id,
+                            title,
+                            latitude,
+                            longitude,
+                            zipcode,
+                            body,
+                            created_by,
+                            created_at
+                        )
+                        FROM posts
+                        ORDER BY id
+                        """
+                    )
+                    result = []
+                    for record in db:
+                        post = PostOut(
+                            id=record[0],
+                            title=record[1],
+                            latitude=record[2],
+                            longitude=record[3],
+                            zipcode=record[4],
+                            body=record[5],
+                            created_by=record[6],
+                            created_at=record[7]
+                        )
+                        result.append(post)
+                    return result
+        except Exception:
+            return {"message": "could not get all posts"}
--- api/routers/posts.py
+++ api/routers/posts.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
-from queries.posts import PostIn, PostRepository
+from queries.posts import PostIn, PostRepository, PostOut, Error
+from typing import Union, List
 
 router = APIRouter()
 
@@ -7,3 +8,10 @@ router = APIRouter()
 @router.post("/api/posts")
 def create_post(post: PostIn, repo: PostRepository = Depends()):
     return repo.create(post)
+
+
+@router.get("/api/posts", response_model=Union[List[PostOut], Error])
+def get_all(
+    repo: PostRepository = Depends(),
+):
+    return repo.get_all()
