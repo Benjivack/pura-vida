b788f46426701c54b8c9c7c8b877a3eedf1e5840
Brian La <brianlax23@gmail.com>
2023-10-12 01:12:31 +0000

    Resolve "Get all reviews for specific trail endpoint"

--- api/queries/reviews.py
+++ api/queries/reviews.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from datetime import date
 from queries.accounts import pool
+from typing import List, Union
 
 
 class Error(BaseModel):
@@ -51,3 +52,73 @@ class ReviewRepository:
                 id = result.fetchone()[0]
                 old_data = reviews.dict()
                 return ReviewOut(id=id, **old_data)
+
+    def get_all(self) -> Union[List[ReviewOut], Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT
+                            id,
+                            body,
+                            rating,
+                            user_id,
+                            post_id,
+                            created_at
+
+                        FROM reviews
+                        ORDER BY created_at
+                        """
+                    )
+                    result = []
+                    for record in db:
+                        review = ReviewOut(
+                            id=record[0],
+                            body=record[1],
+                            rating=record[2],
+                            user_id=record[3],
+                            post_id=record[4],
+                            created_at=record[5]
+                        )
+                        result.append(review)
+                    return result
+        except Exception:
+            return {"message": "could not get all reviews"}
+
+    def get_by_id(self, post_id: int):
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT
+                            id,
+                            body,
+                            rating,
+                            user_id,
+                            post_id,
+                            created_at
+                        FROM reviews
+                        WHERE post_id = %s
+                        """,
+                        [post_id]
+                    )
+                    record = db.fetchall()
+                    print(record)
+                    if record is None:
+                        return {"message": "Post not found"}
+                    rec = []
+                    for r in record:
+                        review = ReviewOut(
+                            id=r[0],
+                            body=r[1],
+                            rating=r[2],
+                            user_id=r[3],
+                            post_id=r[4],
+                            created_at=r[5]
+                        )
+                        rec.append(review)
+                    return rec
+        except Exception:
+            return {"message": "Could not get review by id"}
--- api/routers/reviews.py
+++ api/routers/reviews.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
-from queries.reviews import ReviewIn, ReviewRepository
-
+from queries.reviews import ReviewIn, ReviewRepository, ReviewOut, Error
+from typing import Union, List
 from authenticator import authenticator
 
 router = APIRouter()
@@ -15,3 +15,20 @@ def create_review(
         )
 ):
     return repo.create(review)
+
+
+@router.get("/api/review", response_model=Union[List[ReviewOut], Error])
+def get_all(
+    repo: ReviewRepository = Depends(),
+):
+    return repo.get_all()
+
+
+@router.get(
+        "/api/Review/{post_id}", response_model=Union[List[ReviewOut], Error]
+)
+def get_review_by_id(
+    post_id: int,
+    repo: ReviewRepository = Depends(),
+):
+    return repo.get_by_id(post_id)
